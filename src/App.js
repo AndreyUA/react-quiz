4d81d87404da2f1fe56b4d6f859ae3a076b0e87c
AndreyUA <infernalsteel666@gmail.com>
2020-06-27 12:57:47 +0300

    '6-27-2020'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 
-import Quiz from './containers/Quiz/Quiz';
+import Quiz from "./containers/Quiz/Quiz";
 
 class App extends Component {
   render() {
--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,6 +1,8 @@
 import React from "react";
 import classes from "./ActiveQuiz.module.css";
 
+import AnswersList from './AnswersList/AnswersList';
+
 const ActiveQuiz = (props) => {
   return (
     <div className={classes.ActiveQuiz}>
@@ -10,12 +12,9 @@ const ActiveQuiz = (props) => {
         </span>
         <small>4 из 12</small>
       </p>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
+      <AnswersList
+        answers={props.answers}
+      />
     </div>
   );
 };
